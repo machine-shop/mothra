33bcbb29c834cfd9cad9cf2ad3693cb892d41909
Sera Yang <sera.yang@berkeley.edu>
2018-10-23 14:17:33 -0700

    adding ax=None

---
+++ binarization.py
@@ -0,0 +1,78 @@
+import numpy as np
+from skimage.filters import threshold_otsu
+from scipy import ndimage as ndi
+from skimage.measure import regionprops
+import skimage.color as color
+from skimage.exposure import rescale_intensity
+
+
+
+def find_ruler_edge(binary):
+
+    up_rectangle = int(binary.shape[0]*0.7)
+    left_rectangle = int(binary.shape[1]*0.2)
+    right_rectangle = int(binary.shape[1]*0.4)
+
+    rectangle = np.zeros((binary.shape[0], binary.shape[1]))
+    rectangle[up_rectangle:, left_rectangle: right_rectangle] = 1
+
+    rectangle_binary = binary[up_rectangle:, left_rectangle: right_rectangle]
+    markers, nb_labels = ndi.label(rectangle_binary, structure=ndi.generate_binary_structure(2,1))
+
+    regions = regionprops(markers)
+    areas = [region.area for region in regions]
+
+    idx_max = np.argmax(areas)
+    coords = regions[idx_max].coords
+    offset = np.min(coords[:, 0])
+
+    return up_rectangle + offset
+
+
+def find_label_edge(binary, focus):
+
+	markers = ndi.label(focus, structure=ndi.generate_binary_structure(2,1))[0]
+	regions = regionprops(markers)
+	areas = [region.area for region in regions]
+	area_min = 0.01*binary.shape[0]*binary.shape[1]
+	filtered_regions = []
+	for i, area in enumerate(areas):
+	    if area > area_min:
+	        filtered_regions.append(regions[i])
+	left_pixels = [np.min(region.coords[:, 1]) for region in filtered_regions]
+	crop_right = int(0.5*binary.shape[1] + np.min(left_pixels))
+
+	return crop_right
+
+
+def main(image_rgb, ax=None):
+
+	# find top of ruler
+    image_gray = image_rgb[:, :, 0]
+    thresh = threshold_otsu(image_gray, nbins = 60)
+    binary = image_gray > thresh
+
+    ruler_edge = find_ruler_edge(binary)
+
+    focus = binary[:ruler_edge, int(binary.shape[1]* 0.5):]
+
+    label_edge = find_label_edge(binary, focus)
+
+    bfly_rgb = image_rgb[:ruler_edge, :label_edge]
+    bfly_hsv = color.rgb2hsv(bfly_rgb)[:, :, 1]
+    rescaled = rescale_intensity(bfly_hsv, out_range=(0, 255))
+    tresh = threshold_otsu(rescaled)
+    bfly_bin = rescaled > thresh
+
+    # fig, ax = plt.subplots()
+    if ax:
+        ax.set_title('binary')
+        ax.imshow(bfly_bin)
+
+    return bfly_bin
+
+
+# image_rgb = imread('../data/Measured_images_Data_H.comma/BMNHE_502326.JPG')
+# result = main(image_rgb)
+# plt.imshow(result)
+# plt.show(block=True)
---
+++ ruler_detection.py
@@ -0,0 +1,94 @@
+from skimage.filters import threshold_otsu
+from skimage.measure import regionprops
+import numpy as np
+from scipy import ndimage as ndi
+import cmath
+
+RULER_TOP = 0.7
+RULER_LEFT = 0.2
+RULER_RIGHT = 0.4
+FIRST_INDEX_THRESHOLD = 0.9
+HEIGHT_FOCUS = 400
+LINE_WIDTH = 40
+
+"""
+    Converts image to binary
+"""
+def grayscale(img):
+    image_gray = img[:, :, 0]
+    thresh = threshold_otsu(image_gray, nbins = 60)
+    binary = image_gray > thresh
+    return binary
+
+"""
+    Returns binary rectangle of segment of ruler were interested in
+"""
+def binarize_rect(up_rectangle, binary):
+    left_rectangle = int(binary.shape[1] * RULER_LEFT)
+    right_rectangle = int(binary.shape[1] * RULER_RIGHT)
+
+    rectangle = np.zeros((binary.shape[0], binary.shape[1]))
+    rectangle[up_rectangle:, left_rectangle: right_rectangle] = 1
+
+    rectangle_binary = binary[up_rectangle:, left_rectangle: right_rectangle]
+    return rectangle_binary
+
+"""
+    Performs a fourier transform to find the frequency and t space
+"""
+def fourier(sums):
+    fourier = np.fft.fft(sums)
+    mod = [cmath.polar(el)[0] for el in fourier]
+    freq = np.fft.fftfreq(len(sums))
+
+    idx_max = np.argmax(mod[1:]) + 1
+    f_space = freq[idx_max] # nb patterns per pixel
+    t_space = 1/f_space
+    return t_space
+
+def main(img, ax=None):
+    binary = grayscale(img)
+
+    up_rectangle = int(binary.shape[0] * RULER_TOP)
+    rectangle_binary = binarize_rect(up_rectangle, binary)
+    markers, nb_labels = ndi.label(rectangle_binary, structure=ndi.generate_binary_structure(2,1))
+
+    regions = regionprops(markers)
+    areas = [region.area for region in regions]
+
+    idx_max = np.argmax(areas)
+    coords = regions[idx_max].coords
+    offset = np.min(coords[:, 0])
+
+    # Focusing on the ticks
+    up_focus = up_rectangle + offset + 60
+    left_focus = int(binary.shape[1]*0.1)
+    right_focus = int(binary.shape[1]*0.9)
+    focus = ~binary[up_focus: up_focus + HEIGHT_FOCUS, left_focus: right_focus]
+
+    sums = np.sum(focus, axis=0)/float(HEIGHT_FOCUS)
+
+    first_index = np.argmax(sums > FIRST_INDEX_THRESHOLD)
+
+    t_space = abs(fourier(sums))
+
+    # fig, ax = plt.subplots(figsize=(200, 50))
+    if ax:
+        ax.imshow(img)
+
+        x_single = [left_focus + first_index, left_focus + first_index + t_space]
+        y = np.array([up_focus, up_focus])
+        ax.fill_between(x_single, y, y+LINE_WIDTH, color='red')
+
+        x_mult = [left_focus + first_index, left_focus + first_index + t_space*10]
+        ax.fill_between(x_mult, y-LINE_WIDTH, y, color='blue')
+    return t_space
+
+# if __name__ == '__main__':
+#     name = "BMNHE_502320.JPG"
+#     image_name = "./pictures/"+name
+#     img = imread(image_name)
+#     t_space, ax = main(img)
+#     print "T: ", t_space
+#     plt.savefig("./output/"+name)
+#     plt.close()
