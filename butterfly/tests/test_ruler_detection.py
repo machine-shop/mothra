f0160381c64fc67c70f0064a073e953e34f0da2b
Sera Yang <sera.yang@berkeley.edu>
2018-10-31 21:22:52 -0700

    adding test for binary_rect in ruler_detection

--- butterfly/tests/test_ruler_detection.py
+++ butterfly/tests/test_ruler_detection.py
@@ -3,6 +3,11 @@ import numpy as np
 from numpy import testing as nt
 
 
+RULER_TOP = 50
+RULER_LEFT = 0.2
+RULER_RIGHT = 0.4
+
+
 def test_grayscale():
     input = np.array([[[1, 1, 1],
                       [2, 2, 2]],
@@ -32,8 +37,15 @@ def test_fourier():
     nt.assert_equal(x100, -32)
 
 
+def test_binary_rect():
+    data = np.full((500, 500, 3), 0.0)
+    data[450:500, :, :] += 0.6
+    expected = np.full((50, 100, 3), 0.6)
+    nt.assert_equal(rd.binarize_rect(450, data), expected)
+
+
 def test_main_ruler_detection():
-    data = np.ones((500, 500))
-    data[350:500, 0:500:10] = 0
+    data = np.ones((500, 500, 3))
+    data[350:500, 0:500:10, :] = 0
     t_space, top_space = rd.main(data, None)
     nt.assert_almost_equal(t_space, 10, decimal=0)
