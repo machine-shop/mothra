8d99a2f043bd4c6668208af0f2ad68c2364906aa
theo <theo.bodrito@hotmail.fr>
2018-11-20 14:45:44 -0800

    Change method for detecting points of interest
    - Remove boundary_tracing and fourier_descriptors
    - Implement a new method base on labels and regions for detecting POI:
        - Fix antennae issues
        - Fix bump on wing issues
        - Faster (-2s for each image)
        - More reliable
    - Update test_tracing.py

--- butterfly/tests/test_tracing.py
+++ butterfly/tests/test_tracing.py
@@ -1,77 +1,76 @@
 import numpy as np
 import scipy.ndimage as ndi
 from skimage.measure import regionprops
+import matplotlib.pyplot as plt
 import pytest
+from numpy.testing import assert_array_equal
 
 from butterfly import tracing
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def fake_butterfly():
-    butterfly = np.zeros((100, 200))
-    butterfly[25:50, 25:50] = 1  # left wing
-    butterfly[25:50, 150:175] = 1  # right wing
-    butterfly[50:90, 40:160] = 1  # body
-    butterfly[25:50, 80:120] = 1  # head
+    butterfly = np.zeros((1000, 2000))
+    butterfly[250:500, 250:500] = 1  # left wing
+    butterfly[250:500, 1500:1750] = 1  # right wing
+    butterfly[500:900, 400:1600] = 1  # body
+    butterfly[250:500, 800:1200] = 1  # head
 
     return butterfly
 
 
-@pytest.fixture(scope="module")
-def fake_shape():
-    shape = np.zeros((5, 5))
-    shape[1:, 4] = 1
-    shape[2:4, 2] = 1
-    shape[[1, 4], 3] = 1
-
-    return shape
-
-
-def test_moore_neighborhood():
-    array = np.array([[10, 9], [9, 9], [9, 10], [9, 11],
-                      [10, 11], [11, 11], [11, 10], [11, 9]])
-    assert np.all(tracing.moore_neighborhood([10, 10], [10, 9]) == array)
-
-
 def test_split(fake_butterfly):
-    assert tracing.split_picture(fake_butterfly) in [99, 100]
-
-
-def test_boundary_tracing(fake_shape):
-    bound1 = np.array([[1, 3], [1, 4], [2, 4], [3, 4], [4, 4],
-                       [4, 3], [3, 4], [2, 4]])
-    bound2 = np.array([[1, 3], [1, 4], [2, 4], [3, 4], [4, 4],
-                       [4, 3], [3, 2], [2, 2]])
-    labels1, _ = ndi.label(fake_shape,
-                           structure=ndi.generate_binary_structure(2, 1))
-    labels2, _ = ndi.label(fake_shape,
-                           structure=ndi.generate_binary_structure(2, 2))
-
-    regions1 = regionprops(labels1)
-    regions2 = regionprops(labels2)
-
-    boundary1 = tracing.boundary_tracing(regions1[0])
-    boundary2 = tracing.boundary_tracing(regions2[0])
-
-    assert np.all(boundary1 == bound1)
-    assert np.all(boundary2 == bound2)
-
-
-def test_detect_points_interest(fake_butterfly):
     middle = tracing.split_picture(fake_butterfly)
-    fake_butterfly[:, middle] = 0
-    labels, _ = ndi.label(fake_butterfly,
-                          structure=ndi.generate_binary_structure(2, 2))
-    regions = regionprops(labels)
-    boundary_l = tracing.boundary_tracing(regions[0])
-    boundary_r = tracing.boundary_tracing(regions[1])
+    assert middle == 999
 
-    test_poi_l = np.array([[25, 25], [49, 80]])
-    test_poi_r = np.array([[25, 174], [49, 119]])
-    poi_l = tracing.detect_points_interest(boundary_l, 'l', 200)
-    poi_r = tracing.detect_points_interest(boundary_r, 'r', 200)
 
-    assert np.all(poi_r[0] == test_poi_r[0])
-    assert np.all(poi_l[0] == test_poi_l[0])
-    assert np.all(poi_r[1] == test_poi_r[1])
-    assert np.all(poi_l[1] == test_poi_l[1])
+def test_remove_antenna(fake_butterfly):
+    middle = tracing.split_picture(fake_butterfly)
+
+    binary_left = fake_butterfly[:, :middle]
+    binary_right = fake_butterfly[:, middle:]
+
+    # Adding antennae
+    binary_left[260, 500:800] = 1
+    binary_right[260, 201:501] = 1
+
+    without_antenna_l = tracing.remove_antenna(binary_left)
+    without_antenna_r = tracing.remove_antenna(binary_right)
+
+    assert np.sum(without_antenna_l[260, 500:800]) == 0
+    assert np.sum(without_antenna_r[260, 201:501]) == 0
+
+
+def test_outer_pix(fake_butterfly):
+    middle = tracing.split_picture(fake_butterfly)
+
+    binary_left = fake_butterfly[:, :middle]
+    binary_right = fake_butterfly[:, middle:]
+
+    outer_pix_l = tracing.detect_outer_pix(binary_left, 'l')
+    outer_pix_r = tracing.detect_outer_pix(binary_right, 'r')
+    outer_pix_r = outer_pix_r + np.array([0, middle])
+
+    assert_array_equal(outer_pix_l, np.array([250, 250]))
+    assert_array_equal(outer_pix_r, np.array([250, 1749]))
+
+
+def test_inner_pix(fake_butterfly):
+
+    middle = tracing.split_picture(fake_butterfly)
+
+    binary_left = fake_butterfly[:, :middle]
+    binary_right = fake_butterfly[:, middle:]
+
+    # Relative outer pixels
+    outer_pix_l = np.array([250, 250])
+    outer_pix_r = np.array([250, 750])
+
+    inner_pix_l = tracing.detect_inner_pix(binary_left, outer_pix_l, 'l')
+    inner_pix_l = inner_pix_l + np.array([0, outer_pix_l[1]])
+
+    inner_pix_r = tracing.detect_inner_pix(binary_right, outer_pix_r, 'r')
+    inner_pix_r = inner_pix_r + np.array([0, middle])
+
+    assert_array_equal(inner_pix_l, np.array([499, 799]))
+    assert_array_equal(inner_pix_r, np.array([499, 1200]))
--- butterfly/tracing.py
+++ butterfly/tracing.py
@@ -1,180 +1,129 @@
 import numpy as np
 from scipy import ndimage as ndi
-from cmath import exp, polar, pi
 from skimage.measure import regionprops
-
+from scipy.ndimage.morphology import binary_dilation
 
 # import matplotlib.pyplot as plt
 
-def moore_neighborhood(current, backtrack):  # y, x
-    operations = np.array([[-1, 0], [-1, 1], [0, 1], [1, 1],
-                           [1, 0], [1, -1], [0, -1], [-1, -1]])
-    neighbors = (current + operations).astype(int)
 
-    for i, point in enumerate(neighbors):
-        if np.all(point == backtrack):
-            # we return the sorted neighborhood
-            return np.concatenate((neighbors[i:], neighbors[:i]))
-    return 0
-
-
-def boundary_tracing(region):
-    # creating the binary image
-    coords = region.coords
-    maxs = np.amax(coords, axis=0)
-    binary = np.zeros((maxs[0] + 2, maxs[1] + 2))
-    x = coords[:, 1]
-    y = coords[:, 0]
-    binary[tuple([y, x])] = 1
-
-    # initilization
-    start = [y[0], x[0]]  # starting point is the most upper left point
-    if (binary[start[0] + 1, start[1]] == 0 and
-            binary[start[0] + 1, start[1] - 1] == 0):
-        backtrack_start = [start[0] + 1, start[1]]
-    else:
-        backtrack_start = [start[0], start[1] - 1]
-
-    current = start
-    backtrack = backtrack_start
-    boundary = []
-    counter = 0
-    while True:
-        neighbors_current = moore_neighborhood(current, backtrack)
-        y = neighbors_current[:, 0]
-        x = neighbors_current[:, 1]
-        idx = np.argmax(binary[tuple([y, x])])
-        boundary.append(current)
-        backtrack = neighbors_current[idx - 1]
-        current = neighbors_current[idx]
-        counter += 1
-
-        if (np.all(current == start) and np.all(backtrack == backtrack_start)):
-            # print('iterations :', counter)
-            break
-    return np.array(boundary)
-
-
-def symetric_list(n):
-    output = []
-    for i in range(n):
-        if i % 2 == 0:
-            output.append(-i / 2)
-        else:
-            output.append((i + 1) / 2)
-
-    return np.array(output).astype(int)
-
-
-def fourier_descriptors(boundary, n_descriptors=15):
-    y = boundary[:, 0]
-    x = boundary[:, 1]
-    complex_boundary = x + y * 1j
-    n = len(boundary)
-    descriptors = []
-    k_values = symetric_list(n_descriptors)
-    for p in range(n_descriptors):
-        sum_c = 0
-        k = k_values[p]
-        for i in range(n):
-            sum_c += complex_boundary[i] * exp(-2 * pi * 1j * (i + 1) * k / n)
-        descriptors.append(round((sum_c / n).real, 3)
-                           + round((sum_c / n).imag, 3) * 1j)
-    return descriptors
-
-
-def normalize_descriptors(descriptors):
-    mod_c1 = polar(descriptors[1])[0]
-    return ([round(polar(descriptor)[0] / mod_c1, 4)
-             for descriptor in descriptors[2:]])
-
-
-def inv_fourier(descriptors, n_points=1000):
-    k_values = symetric_list(len(descriptors))
-    x = []
-    y = []
-    for i in range(n_points):
-        z = 0
-        for p in range(len(descriptors)):
-            k = k_values[p]
-            z += descriptors[p] * exp((2 * pi * 1j * k * i) / n_points)
-        z = int(z.real) + int(z.imag) * 1j
-        x.append(z.real)
-        y.append(z.imag)
-
-    x = np.array(x).astype(int)
-    y = np.array(y).astype(int)
-
-    return y, x
-
-
-def detect_points_interest(smooth_boundary, side, width_cropped):
-    """Calculate the outer and inner points of interest of the wing
+def remove_antenna(half_binary):
+    """Remove antenna if connected to the wing
 
     Arguments
     ---------
-    smooth_boundary : 2D numpy array [[y1, x1], [y2, x2] .. ]
-        Clockwise coordinates of the smoothed boundary, starting
-        with the highest pixel.
-    side : str 'l' or 'r'
-        'left' or 'right' wing
-    width_cropped : int
-        width og the cropped butterfly in pixels
+    half_binary : 2D array
+        binary image of left/right wing
 
     Returns
     -------
-    outer_pixel : 1D numpy array [y, x]
-        Coordinates of the boundary closest point to the top right/left corner
-    inner_pixel : 1D numpy array [y, x]
-        Coordinates of the top of the junction between body and wing
+    without_antenna : 2D array
+        binary image, same shape as input without antenna (if it touches the
+        wing)
+    """
+
+    markers, _ = ndi.label(1-half_binary, ndi.generate_binary_structure(2, 1))
+    regions = regionprops(markers)
+
+    areas = np.array([r.area for r in regions])
+    idx_sorted = 1 + np.argsort(-areas)[:2]
+
+    try:
+        dilated_bg = binary_dilation(markers == idx_sorted[0], iterations=35)
+        dilated_hole = binary_dilation(markers == idx_sorted[1], iterations=35)
+        intersection = np.minimum(dilated_bg, dilated_hole)
+        without_antenna = np.copy(half_binary)
+        without_antenna[intersection] = 0
+    except IndexError:
+        return half_binary
+
+    return without_antenna
+
+
+def detect_outer_pix(half_binary, side):
+    """Relative (r, c) coordinates of outer pixel (wing's tip)
+
+    Arguments
+    ---------
+    half_binary : 2D array
+        binary image of left/right wing
+    side : str
+        left ('l') or right ('r') wing
+
+    Returns
+    -------
+    outer_pix : 1D array
+        relative coordinates of the outer pixel (r, c)
     """
     if side == 'r':
-        corner = np.array([0, width_cropped])
+        width = half_binary.shape[1]
+        corner = np.array([0, width])
     else:
         corner = np.array([0, 0])
 
-    # we look for the boundary closest point to the top right/ left corner
-    # as our starting point
-    distances = np.linalg.norm(smooth_boundary - corner, axis=1)
-    idx_start = np.argmin(distances)
-    outer_pixel = smooth_boundary[idx_start]
-    if side == 'r':
-        ordered_boundary = np.concatenate((smooth_boundary[idx_start + 1:],
-                                           smooth_boundary[:idx_start + 1]),
-                                          axis=0)
-        ordered_boundary = np.flip(ordered_boundary, axis=0)
+    markers, _ = ndi.label(half_binary,
+                           ndi.generate_binary_structure(2, 1))
+    regions = regionprops(markers)
+    areas = [r.area for r in regions]
+    idx_max = np.argmax(areas)
+
+    coords = regions[idx_max].coords
+    distances = np.linalg.norm(coords - corner, axis=1)
+    idx_outer_pix = np.argmin(distances)
+    outer_pix = coords[idx_outer_pix]
+
+    return outer_pix
+
+
+def detect_inner_pix(half_binary, outer_pix, side):
+    """Relative (r, c) coordinates of the inner pixel (between wing and body)
+
+    Arguments
+    ---------
+    half_binary : 2D array
+        binary image of left/right wing
+    outer_pix : 1D array
+        (r, c) coordinates (relative) of the outer pixel
+    side : str
+        left ('l') or right ('r') wing
+
+    Returns
+    -------
+    inner_pix : 2D array
+        relative coordinates of the inner pixel (r, c)
+    """
+    lower_bound = int(half_binary.shape[0]*0.75)
+
+    if side == 'l':
+        focus = half_binary[:lower_bound, outer_pix[1]:]
     else:
-        ordered_boundary = np.concatenate((smooth_boundary[idx_start:],
-                                           smooth_boundary[:idx_start]),
-                                          axis=0)
+        focus = half_binary[:lower_bound, :outer_pix[1]]
 
-    current_idx = 50
-    step = 1
-    iterations = 1
-    if side == 'r':
-        coeff = 1
+    focus_inv = 1 - focus
+
+    markers, _ = ndi.label(focus_inv, ndi.generate_binary_structure(2, 1))
+    regions = regionprops(markers)
+    areas = [r.area for r in regions]
+    idx_max = np.argmax(areas)
+    coords = regions[idx_max].coords
+    y_max = np.max(coords[:, 0])
+    mask = (coords[:, 0] == y_max)
+    selection = coords[mask]
+    if side == 'l':
+        idx = np.argmax(selection[:, 1])
     else:
-        coeff = -1
-    while True:
-        current_pixel = ordered_boundary[current_idx]
-        next_idx = current_idx + step
-        next_pixel = ordered_boundary[next_idx]
-        if (current_pixel[0] > next_pixel[0]
-                or coeff * current_pixel[1] < coeff * next_pixel[1]):
-            print('iterations :', iterations)
-            inner_pixel = next_pixel
-            break
-        iterations += 1
-        current_idx = next_idx
-    return outer_pixel, inner_pixel
+        idx = np.argmin(selection[:, 1])
+
+    outer_pix = selection[idx]
+
+    return outer_pix
 
 
-def split_picture(closed):
+def split_picture(binary):
     """Calculate the middle of the butterfly.
 
     Parameters
     ----------
-    closed : ndarray of bool
+    binary : ndarray of bool
         Binary butterfly mask.
 
     Returns
@@ -188,7 +137,7 @@ def split_picture(closed):
     of gravity of the butterfly.
     """
 
-    means = np.mean(closed, 0)
+    means = np.mean(binary, 0)
     normalized = means / np.sum(means)
     sum_values = 0
     for i, value in enumerate(normalized):
@@ -196,7 +145,7 @@ def split_picture(closed):
     return int(sum_values)
 
 
-def main(binary, ax):
+def main(binary, ax=None):
     """Find and retunrs the coordinates of the 4 points of interest
 
     Arguments
@@ -212,72 +161,40 @@ def main(binary, ax):
     points_interest : 2D array
         array of coordinates of the points of interest in the form [y, x]
         [outer_pix_l, inner_pix_l, outer_pix_r, inner_pix_r]
-
-    Notes
-    -----
-    This satge can be split in 4 different parts :
-    1. Split the butterfly to separate the two wings
-    2. Find the boundaries of each wing
-    3. Smooth those boundaries with Fourier transformation
-    4. Detect the points of interest from the smoothed boundaries
-
     """
+    binary = binary_dilation(binary, iterations=2)
 
-    # 1. Split the butterfly
-    half = split_picture(binary)
-    # print(half)
+    # Split the butterfly
+    middle = split_picture(binary)
 
-    divided = np.copy(binary)
-    divided[:, half:half + 5] = 0
+    binary_left = binary[:, :middle]
+    binary_right = binary[:, middle:]
 
-    # Detecting the wing regions
-    markers_divided, _ = ndi.label(divided,
-                                   ndi.generate_binary_structure(2, 1))
-    regions = regionprops(markers_divided)
-    areas = [region.area for region in regions]
+    # Left wing
+    without_antenna_l = remove_antenna(binary_left)
+    outer_pix_l = detect_outer_pix(without_antenna_l, 'l')
+    inner_pix_l = detect_inner_pix(without_antenna_l, outer_pix_l, 'l')
+    inner_pix_l = inner_pix_l + np.array([0, outer_pix_l[1]])
 
-    idx_1 = np.argmax(areas)
-    coords1 = regions[idx_1].coords
-    areas[idx_1] = 0
-    idx_2 = np.argmax(areas)
-    coords2 = regions[idx_2].coords
+    # Right wing
+    without_antenna_r = remove_antenna(binary_right)
+    outer_pix_r = detect_outer_pix(without_antenna_r, 'r')
+    inner_pix_r = detect_inner_pix(without_antenna_r, outer_pix_r, 'r')
+    inner_pix_r = inner_pix_r + np.array([0, middle])
+    outer_pix_r = outer_pix_r + np.array([0, middle])
 
-    # Determining which one is left or right
-    if np.min(coords1[:, 1]) < np.min(coords2[:, 1]):
-        region_l, region_r = regions[idx_1], regions[idx_2]
-    else:
-        region_l, region_r = regions[idx_2], regions[idx_1]
+    points_interest = np.array([outer_pix_l,
+                                inner_pix_l,
+                                outer_pix_r,
+                                inner_pix_r])
 
-    # 2. Find boundaries
-    boundary_l = boundary_tracing(region_l)
-    boundary_r = boundary_tracing(region_r)
-
-    # 3. Smooth boundaries
-    descriptors_l = fourier_descriptors(boundary_l, 45)
-    descriptors_r = fourier_descriptors(boundary_r, 45)
-    smoothed_y_l, smoothed_x_l = inv_fourier(descriptors_l, 1500)
-    smoothed_y_r, smoothed_x_r = inv_fourier(descriptors_r, 1500)
-    smoothed_l = np.concatenate((smoothed_y_l.reshape(-1, 1),
-                                 smoothed_x_l.reshape(-1, 1)),
-                                axis=1)
-    smoothed_r = np.concatenate((smoothed_y_r.reshape(-1, 1),
-                                 smoothed_x_r.reshape(-1, 1)),
-                                axis=1)
-
-    # 4. Detecting points of interest
-    outer_pix_l, inner_pix_l = detect_points_interest(smoothed_l,
-                                                      'l', binary.shape[1])
-    outer_pix_r, inner_pix_r = detect_points_interest(smoothed_r,
-                                                      'r', binary.shape[1])
-
-    points_interest = np.array([outer_pix_l, inner_pix_l,
-                                outer_pix_r, inner_pix_r])
-
+    # Reconstruct binary image without antennae
+    without_antennae = np.concatenate((without_antenna_l, without_antenna_r),
+                                      axis=1)
     if ax:
         ax.set_title('Tracing')
-        ax.imshow(divided)
-        ax.scatter(smoothed_x_l, smoothed_y_l, color='white', s=4)
-        ax.scatter(smoothed_x_r, smoothed_y_r, color='cyan', s=4)
+        ax.imshow(without_antennae)
+        ax.plot([middle, middle], [0, binary.shape[0]-5])
         ax.scatter(points_interest[:, 1],
                    points_interest[:, 0], color='r', s=10)
 
