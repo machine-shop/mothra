07c3292168f32e3770618d4575292b2b903c9c61
denniscfeng <24783183+denniscfeng@users.noreply.github.com>
2019-03-21 22:20:19 -0700

    Result plotting script (#39)
    * currently saves an outlier.csv file listing measurements >1 SD
    
    * result_plotting now takes arguments, and saves either full comparisons or csv of outliers only, improved pandas code
    
    * outlier images are now copied to an outliers/ folder (if the option is set)
    
    * SD argument should be able to take floats
    
    * Updating Readme for new result_plotting features
    
    * Small update about jupyter notebooks in readme
    
    * Fix typo

--- README.md
+++ README.md
@@ -67,14 +67,69 @@ in `/butterfly-wings` will run the pipeline on the example data in `/butterfly-w
 ```
 
 ## Miscellaneous
-
-Jupyter notebooks with prototypes for various methods are located in the `/notebooks` directory.
-
 The testing suite can be run with `PYTHONPATH=. pytest` from `/butterfly-wings`.
 
-## Accuracy
-Run the `result_plotting.py` file to print out to generate a histogram of the difference for all actual - predicted measurements. (Not parametarized, so you will need to edit the script for the correct paths to compare result `.csv`'s). Default expected locations are:
+# Result Plotting
+`result_plotting.py` is a script that generates a histogram of differences between actual measurements and predicted measurements. This is useful for debugging and evaluating accuracy. This can be used in isolation from the main pipeline, and simply takes in the predicted `results.csv` from the pipeline and either an `.xlsx` file or `.csv` file with actual measurements.
+
+## Usage
 ```
-/butterfly-wings/results.csv (generated measurements)
-/butterfly-wings/actual.xlsx (original measurements spreadsheet)
+python result_plotting.py -a "h_comma_wing_lengths.xlsx" -n "full name" -l "Left" -r "Right"
+```
+Apart from outputting a plot of the differences, it can also output a `comparison.csv` with all differences between predicted and actual measurements, and/or a `outliers.csv` with only measurement differences from outliers. It can also copy outlier images to a `outliers/` folder, for easier debugging by rerunning the pipeline on these outlier images.
+
+## Options
+* -a, --actual : File path of either Excel `.xlsx` file or `.csv` file containing the actual measurements.
+* -n, --name : Name of column in `actual` file that contains the image names for each measurement.
+* -l, --left : Name of column in `actual` file that contains the name of left wing measurements.
+* -r, --right : Name of column in `actual` file that contains the name of right wing measurements.
+* -p, --predicted : File path of `.csv` predictions outputted by out pipeline. `results.csv` by default.
+* -c, --comparison : If specified, will output a `comparison.csv` file containing all measurements and the differences.
+* -o, --outliers : If specified, will output a `outliers.csv` file containing only measurements that are deemed outliers.
+* -sd, --sd : By default, the SD to determine an outlier is +/- 2 SD's away from the average measurement. If specified, you can use something else.
+* -co, --copy_outliers : Specify a folder where the outlier images are from, and copy any outlier images to a `outliers/` folder in the current directory.
+
+## Example
+Example files:
+```
+/"Measured_images_Data_H.comma"
+    BMNHE_1354218.JPG
+    ...
+    ...
+/butterfly-wings
+    result_plotting.py
+    h_comma_wing_lengths.xlsx
+    results.csv
+```
+`h_comma_wing_lengths.xlsx` has these columns:
+
+| full name         | ... | Right | Left   |   |
+|-------------------|-----|-------|--------|---|
+| BMNHE_1354218.JPG | ... | 15.11 | 15.288 |   |
+| ...               | ... | ...   | ...    |   |
+|                   |     |       |        |   |
+
+Thus, we will specify `-n "full name"`, `-l "Left"`, `-r "Right"`.
+
+We know what `results.csv` looks like (since it is output by the pipeline).
+
+We also want to output both the `comparison.csv` and `outliers.csv` table, so we will specify `-c` and `-o`, and we also want to copy outliers from the images folder `Measured_images_Data_H.comma`, so we will do `-co "../Measured_images_Data_H.comma"`. We also want to define an outlier as a measurement +-1.75 SD's from the actual measurement, so we well specify `-sd 1.75`. Thus the usage is:
+
+```
+python result_plotting.py -a "h_comma_wing_lengths.xlsx" -n "full name" -l "Left" -r "Right" -c -o -co "../Measured_images_Data_H.comma" -sd 1.75 
+```
+
+File results:
+```
+/butterfly-wings
+    result_plotting.py
+    h_comma_wing_lengths.xlsx
+    results.csv
+    ...
+    result_plot.png
+    comparison.csv
+    outliers.csv
+    /outliers
+        BMNHE_1354218.JPG (an outlier image)
+        ...
 ```
--- result_plotting.py
+++ result_plotting.py
@@ -1,50 +1,150 @@
 import os
+import shutil
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-RESULTS_INPUT = 'results.csv'
-ACTUAL_INPUT = 'actual.xlsx'
+import argparse
 
-# Reads in the files and parses out only the useful columns
-actual = pd.read_excel(ACTUAL_INPUT)
-predicted = pd.read_csv(RESULTS_INPUT)
-actual = actual[['full name', 'Right', 'Left']]
-actual = actual.rename(index=str, columns={"Right": "actual_right",
-                                           "Left": "actual_left"})
-predicted = predicted.rename(index=str, columns={"right_wing (mm)":
-                                                 "predicted_right",
-                                                 "left_wing (mm)":
-                                                 "predicted_left"})
+# Argument parsing
+parser = argparse.ArgumentParser(description='Script to plot differences between actual butterfly measurements versus predicted measurements.')
+parser.add_argument('-a', '--actual',
+                    type=str,
+                    help='File name with actual measurements',
+                    required=True)
+parser.add_argument('-n', '--name',
+                    type=str,
+                    help='Name of column containing file name of each measured image',
+                    required=True)
+parser.add_argument('-l', '--left',
+                    type=str,
+                    help='Name of column containing left wing measurements in actual measurements file',
+                    required=True)
+parser.add_argument('-r', '--right',
+                    type=str,
+                    help='Name of column containing right wing measurements in actual measurements file',
+                    required=True)
+parser.add_argument('-p', '--predicted',
+                    type=str,
+                    help='File name with predicted measurement results (assumed to be a csv produced by our pipeline)',
+                    default='results.csv')
+parser.add_argument('-c', '--comparison',
+                    action='store_true',
+                    help='Produce an "comparison.csv" file containing all measurements and the differences')
+parser.add_argument('-o', '--outliers',
+                    action='store_true',
+                    help='Produce an "outliers.csv" file containing only measurements that are deemed outliers')
+parser.add_argument('-sd', '--sd',
+                    type=float,
+                    help="The number of SD's that are used as a theshold for classifying an outlier",
+                    default=2)
+parser.add_argument('-co', '--copy_outliers',
+                    type=str,
+                    help='Create a folder "outliers" and copy images from the specified path that correspond to outliers')
+args = parser.parse_args()
+
+
+# Reading in both actual measurements file, and select desired columns
+actual_file, actual_ext = os.path.splitext(args.actual)
+actual = pd.DataFrame()
+if actual_ext.lower() == ".xlsx":
+    actual = pd.read_excel(args.actual)
+elif actual_ext.lower() == ".csv":
+    actual = pd.read_csv(args.actual)
+actual = actual[[args.name, args.left, args.right]]
+actual.rename(columns={args.name: "image_id", args.left: "actual_left", args.right: "actual_right"}, inplace=True)
+
+
+# Reading in predicted results from csv
+predicted = pd.read_csv(args.predicted)
+predicted.rename(columns={"left_wing (mm)": "predicted_left", "right_wing (mm)": "predicted_right"}, inplace=True)
+
 
 # Merging them together and creating new columns for the difference
-both = pd.merge(actual, predicted, left_on='full name',
-                right_on='image_id').drop(['image_id'], axis=1)
+both = pd.merge(actual, predicted, on="image_id", how='inner')
 both['left_diff'] = both['predicted_left'] - both['actual_left']
 both['right_diff'] = both['predicted_right'] - both['actual_right']
-all_diffs = both['right_diff'].append(both['left_diff'])
 
-# Outliers
+all_diffs = both['right_diff'].append(both['left_diff'])
 mean = np.mean(all_diffs)
 sd = np.std(all_diffs)
-lower = mean - 2 * sd
-upper = mean + 2 * sd
-print(f"Mean: {mean} SD: {sd}.")
-print(f"Lower: {lower} Upper: {upper}.")
 
-outliers = all_diffs[(all_diffs < lower) | (all_diffs > upper)]
-print(f"Num outliers: {len(outliers)}")
-all_diffs = all_diffs[(all_diffs > lower) & (all_diffs < upper)]
+both['left_SD'] = (both['left_diff'] - mean)/sd
+both['right_SD'] = (both['right_diff'] - mean)/sd
+both['is_outlier'] = (abs(both['left_SD'])>args.sd) | (abs(both['right_SD'])>args.sd)
 
+
+# Print statistics about differences
+lower = mean - args.sd * sd
+upper = mean + args.sd * sd
+num_outlier_measurements = len(all_diffs[(all_diffs < lower) | (all_diffs > upper)])
+num_outlier_images = np.count_nonzero(both['is_outlier'])
+print("DIFFERENCE STATISTICS")
+print(f"    Mean Differences: {mean}")
+print(f"    Differences SD: {sd}.")
+print(f"    Lower Bound (-{args.sd} SD) of Differences: {lower}")
+print(f"    Upper Bound (+{args.sd} SD) of Differences: {upper}")
+print(f"    Number of outlying measurements: {num_outlier_measurements}")
+print(f"    Number of images with outlying measurements: {num_outlier_images}")
+print("")
+
+
+# Plot histogram of differences
+all_diffs_nonoutlier = all_diffs[(all_diffs >= lower) & (all_diffs <= upper)]
 fig, ax = plt.subplots(figsize=(10, 5))
-ax = all_diffs.hist(bins='auto')
+ax = all_diffs_nonoutlier.hist(bins='auto')
+
 
 # Saving the plot
 filename = 'result_plot.png'
 output_path = os.path.normpath(filename)
 plt.xlabel('Difference between (predicted - actual) in mm')
 start, end = ax.get_xlim()
-plt.ylabel('Number of samples')
-plt.title('Error in predicted length')
+plt.ylabel('Number of images')
+plt.title('Error in predicted measurements')
 plt.savefig(output_path)
 plt.close()
+print(f"Saved plot of differences to {filename}")
+
+
+# Printing either full comparison csv or outliers csv
+both['SD_sum'] = abs(both['left_SD']) + abs(both['left_SD'])
+both.sort_values('SD_sum', ascending=False, inplace=True)
+both.drop('SD_sum', axis=1, inplace=True)
+both.sort_values('is_outlier', ascending=False, inplace=True, kind='mergesort')
+
+if args.comparison:
+    comparison_filename = 'comparison.csv'
+    outlier_col_str = both["is_outlier"].replace({True:"TRUE", False:""})
+    both_outlier_col_str = both.copy()
+    both_outlier_col_str["is_outlier"] = outlier_col_str
+    both_outlier_col_str.to_csv(comparison_filename)
+    print(f"Saved all differences to {comparison_filename}")
+
+if args.outliers:
+    outliers_filename = 'outliers.csv'
+    both_outliers_only = both[both['is_outlier']].copy()
+    both_outliers_only.drop('is_outlier', axis=1, inplace=True)
+    both_outliers_only.to_csv(outliers_filename)
+    print(f'Saved {num_outlier_images} rows to {outliers_filename}')
+
+# Fetching outlier images
+if args.copy_outliers:
+    outliers_folder = 'outliers/'
+    if os.path.exists(outliers_folder):
+        oldList = os.listdir(outliers_folder)
+        for oldFile in oldList:
+            os.remove(os.path.join(outliers_folder, oldFile))
+    else:
+        os.mkdir(outliers_folder)
+
+    image_list = both[both['is_outlier']]['image_id']
+    print(f'Copying {len(image_list)} outlier images to {outliers_folder} ...', end="")
+
+    for image_name in image_list:
+        image_path = os.path.join(args.copy_outliers, image_name)
+        shutil.copy(image_path, outliers_folder)
+
+    print("done")
+
