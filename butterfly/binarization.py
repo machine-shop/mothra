d4e10b3bc69fdcd7305dc5d921a1f3596a69f725
alexdesiqueira <alex.desiqueira@igdore.org>
2021-07-28 16:46:24 -0700

    Adapting and refactoring binarization, v1

--- butterfly/binarization.py
+++ butterfly/binarization.py
@@ -8,162 +8,128 @@ from skimage.morphology import binary_erosion, binary_dilation, selem
 from skimage.transform import rescale
 from skimage.util import img_as_bool, img_as_float32, img_as_ubyte
 from joblib import Memory
-from fastai.vision import load_learner, Image
+from fastai.vision.core import PILImage, to_image
+from fastai.vision.learner import load_learner
 from pathlib import Path
-from skimage.io import imsave
 from torch import from_numpy
-from butterfly import connection
+from butterfly import connection, ruler_detection
 
 location = './cachedir'
 memory = Memory(location, verbose=0)
-
-# Height of a square region used to determine the minimum region size in find_tags_edge.
-# Percent of height of the image.
-MIN_REGION_HEIGHT = 0.05
 
 # Height of extra margin to make sure all of the ruler is cropped out in find_tags_edge.
 # Percent of height of the image
 RULER_CROP_MARGIN = 0.025
 
-# Distance from right-hand edge of the image in which we consider regions to be tags.
-# Used in find_tags_edge. Percent of width of the image
-REGION_CUTOFF = 1/3
+# Testing weights for segmentation of four classes (background, tags, ruler,
+# Lepidoptera).
+WEIGHTS_SEGMENTATION = './models/segmentation_test-4classes.pkl'
 
 
 def _convert_image_to_tensor(image):
     """Auxiliary function. Receives an RGB image and convert it to be processed
     by fastai."""
-    image = img_as_float32(np.transpose(image, axes=(2, 0, 1)))
-    tensor = Image(from_numpy(image))
+    #image = img_as_float32(np.transpose(image, axes=(2, 0, 1)))
+    tensor = to_image(img_as_float32(image))
+    #tensor = PILImage.create(from_numpy(image))
 
     return tensor
 
 
-def find_tags_edge(image_rgb, top_ruler, axes=None):
+def _rescale_image(image_refer, image_to_rescale):
+    """Helper function. Rescale image back to original size, according to
+    reference."""
+    scale_ratio = np.asarray(image_refer.shape[:2]) / np.asarray(image_to_rescale.shape)
+    return rescale(image=image_to_rescale, scale=scale_ratio)
+
+
+def find_tags_edge(tags_bin, top_ruler, axes=None):
     """Find the edge between the tag area on the right and the butterfly area
     and returns the corresponding x coordinate of that vertical line
 
-    Arguments
-    ---------
-    image_rgb : (M, N, 3) ndarray
-        Full RGB image input image
+    Parameters
+    ----------
+    tags_bin : (M, N) ndarray
+        Binary image contaning the tags.
     top_ruler : int
-        Y-coordinate of the top of the ruler
+        Y-coordinate of the top of the ruler.
+    axes : obj
+        If any, detected tags will be plotted on it.
 
     Returns
     -------
-    label_edge : int
-        x coordinate of the vertical line separating the tags area from the
-        butterfly area
+    first_tag_edge : int
+        X coordinate of the vertical line separating the tags area from the
+        butterfly area.
     """
-
-    # Make sure ruler is cropped out with some extra margin
-    image_rgb = image_rgb[:top_ruler - int(RULER_CROP_MARGIN*image_rgb.shape[0])]
-
-    # Binarize the image with rgb2hsv to highlight the butterfly
-    img_hsv = color.rgb2hsv(image_rgb)[:, :, 1]
-    img_hsv_rescaled = rescale_intensity(img_hsv, out_range=(0, 255))
-    img_hsv_thresh = threshold_otsu(img_hsv_rescaled)
-    img_bfly_bin = img_hsv_rescaled > img_hsv_thresh
-
-    # Fill holes and erode the butterfly to get clean butterfly region
-    img_bfly_bin_filled = ndi.binary_fill_holes(img_bfly_bin)
-    img_bfly_bin_filled_eroded = binary_erosion(img_bfly_bin_filled)
-
-
-    # Binarize the image with otsu to highlight the labels/ruler
-    img_gray = image_rgb[:, :, 0]
-    img_otsu_thresh = threshold_otsu(img_gray, nbins=60)
-    img_tags_bin = img_gray > img_otsu_thresh
-
-    # Fill holes and erode tags to get clean regions
-    img_tags_filled = ndi.binary_fill_holes(img_tags_bin)
-    img_tags_filled_eroded = binary_erosion(img_tags_filled)
-
-
-    # Combine clean butterfly and tags images
-    max_img = np.max([img_bfly_bin_filled_eroded, img_tags_filled_eroded], axis=0)
+    # Make sure ruler is cropped out with some extra margin.
+    tags_bin = tags_bin[:top_ruler - int(RULER_CROP_MARGIN * tags_bin.shape[0])]
 
     # Calculate regionprops
-    max_img_markers, max_img_labels = ndi.label(max_img)
-    max_img_regions = regionprops(max_img_markers)
+    tags_regions = regionprops(label(tags_bin))
 
-
-    # For all notable regions (large, and and in the right third of the image), get their distance to top left corner (0, 0)
-    smallest_area = (MIN_REGION_HEIGHT * max_img.shape[0]) ** 2
-    max_img_focus_regions = [r for r in max_img_regions if r.area>smallest_area]
-    max_img_region_disttocorner = [np.linalg.norm(r.centroid) for r in max_img_focus_regions]
-
-    # Using those, find the ruler and butterfly and ignore them. The remaining regions are tags
-    bfly_region = max_img_focus_regions[np.argsort(max_img_region_disttocorner)[0]]
-    max_img_focus_regions.remove(bfly_region)
-
-    # To remove ambiguity what is a tag, only look at the right REGION_CUTOFF percent of the image for tags
-    cutoff = (1-REGION_CUTOFF) * max_img.shape[1]
-    max_img_focus_cutoff_regions = [r for r in max_img_focus_regions if r.centroid[1]>cutoff]
-
-    # From the remaining regions find their leftmost edge
-    max_img_leftedges = [r.bbox[1] for r in max_img_focus_cutoff_regions] + [max_img.shape[1]]
-    # Binary erosion causes a pixel to be eroded away from the tag edge
-    label_edge = np.min(max_img_leftedges) - 1
-
+    # checking where the first tag starts.
+    tags_leftedges = [region.bbox[1] for region in tags_regions]
+    first_tag_edge = np.min(tags_leftedges)
 
     if axes and axes[6]:
-        halfway = img_tags_filled_eroded.shape[1]//2
-        axes[6].imshow(img_tags_filled_eroded[:, halfway:])
-        axes[6].axvline(x=label_edge-halfway, color='c', linestyle='dashed')
+        halfway = tags_bin.shape[1] // 2
+        axes[6].imshow(tags_bin[:, halfway:])
+        axes[6].axvline(x=first_tag_edge-halfway,
+                        color='c',
+                        linestyle='dashed')
         axes[6].set_title('Tags detection')
 
-    return label_edge
+    return first_tag_edge
 
 
-def unet_binarization(bfly_rgb, weights='./models/segmentation.pkl'):
-    """Extract shape of the butterfly using the U-net neural network.
+def binarization(image_rgb, weights=WEIGHTS_SEGMENTATION):
+    """Extract the shape of the elements in an input image using the U-net
+    deep learning architecture.
 
-    Arguments
-    ---------
-    bfly_rgb : (M, N, 3) ndarray
-        Input RGB image of butterfly (ruler and tags cropped out)
+    Parameters
+    ----------
+    image_rgb : (M, N, 3) ndarray
+        Input RGB image of Lepidoptera, with ruler and tags.
 
     Returns
     -------
-    bfly_unet_bin : (M, N) ndarray
-        Resulting binarized image of butterfly after segmentation by U-net.
+    image_bin : (M, N) ndarray
+        Input image binarized.
     """
     if isinstance(weights, str):
         weights = Path(weights)
 
     connection.download_weights(weights)
     # parameters here were defined when training the U-net.
-    learner = load_learner(path=weights.parent, file=weights.name)
+    learner = load_learner(fname=weights)
 
     print('Processing U-net...')
-    bfly_aux = _convert_image_to_tensor(bfly_rgb)
-    _, pred_classes, _ = learner.predict(bfly_aux)
+    _, _, classes = learner.predict(image_rgb)
+    _, tags_bin, ruler_bin, lepidop_bin = classes[:4]
 
-    # rescale the image back up.
-    scale_ratio = np.asarray(bfly_rgb.shape[:2]) / np.asarray(
-        pred_classes[0].shape)
-    bfly_unet_bin = rescale(image=pred_classes[0].numpy().astype('float'),
-                            scale=scale_ratio)
+    # rescale the predicted images back up and binarize them.
+    tags_bin = img_as_bool(_rescale_image(image_rgb, tags_bin))
+    ruler_bin = img_as_bool(_rescale_image(image_rgb, ruler_bin))
+    lepidop_bin = img_as_bool(_rescale_image(image_rgb, lepidop_bin))
 
-    return bfly_unet_bin
+    return tags_bin, ruler_bin, lepidop_bin
 
 
-def return_largest_region(img_bin):
+def return_largest_region(image_bin):
     """Returns the largest region in the input image.
 
     Parameters
     ----------
-    img_bin : (M, N) ndarray
+    image_bin : (M, N) ndarray
         A binary image.
 
     Returns
     -------
-    img_bin : (M, N) ndarray
+    image_bin : (M, N) ndarray
         The input binary image containing only the largest region.
     """
-    props = regionprops(label(img_bin))
+    props = regionprops(label(image_bin))
 
     # largest_reg will receive the largest region label and its area.
     largest_reg = [0, 0]
@@ -171,46 +137,49 @@ def return_largest_region(img_bin):
             if prop.area > largest_reg[1]:
                 largest_reg = [prop.label, prop.area]
 
-    img_bin[label(img_bin) != largest_reg[0]] = 0
+    image_bin[label(image_bin) != largest_reg[0]] = 0
 
-    return img_as_bool(img_bin)
+    return img_as_bool(image_bin)
 
 
 @memory.cache(ignore=['axes'])
-def main(image_rgb, top_ruler, axes=None):
-    """Binarizes and crops properly image_rgb
+def main(image_rgb, axes=None):
+    """Binarizes and crops the Lepidoptera in image_rgb.
 
-    Arguments
-    ---------
+    Parameters
+    ----------
     image_rgb : 3D array
         RGB image of the entire picture
     top_ruler: integer
         Y-coordinate of the height of the ruler top edge as
         found by ruler_detection.py
-    ax : obj
-        If any, the result of the binarization and cropping
-        will be plotted on it
+    axes : obj
+        If any, the binarization result will be plotted on it.
 
     Returns
     -------
-    bfly_bin : 2D array
-        Binarized and cropped version of imge_rgb
+    image_bin : 2D array
+        Binarized version of image_rgb.
     """
-
-    label_edge = find_tags_edge(image_rgb, top_ruler, axes)
-
-    bfly_rgb = image_rgb[:top_ruler, :label_edge]
-
-    # binarizing the input image using U-Net.
-    bfly_bin = unet_binarization(bfly_rgb, weights='./models/segmentation.pkl')
+    # binarizing the input image and separating its elements.
+    lepidop_bin, ruler_bin, tags_bin = binarization(image_rgb, weights=WEIGHTS_SEGMENTATION)
 
     # if the binary image has more than one region, returns the largest one.
-    bfly_bin = return_largest_region(bfly_bin)
+    lepidop_bin = return_largest_region(lepidop_bin)
+
+    # detecting where the ruler starts.
+    _, top_ruler = ruler_detection.main(ruler_bin, axes)
+
+    # detecting where the tags start.
+    first_tag_edge = find_tags_edge(tags_bin, top_ruler, axes)
+
+    # cropping the Lepidoptera.
+    lepidop_bin = lepidop_bin[:top_ruler, :first_tag_edge]
 
     if axes and axes[1]:
-        axes[1].imshow(bfly_bin)
-        axes[1].set_title('Binarized butterfly')
+        axes[1].imshow(lepidop_bin)
+        axes[1].set_title('Binarized Lepidoptera')
     if axes and axes[3]:
-        axes[3].axvline(x=label_edge, color='c', linestyle='dashed')
+        axes[3].axvline(x=first_tag_edge, color='c', linestyle='dashed')
 
-    return bfly_bin
+    return lepidop_bin, ruler_bin, tags_bin
