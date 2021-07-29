4812f825dc8884e8107005e0472dd28e2683af2c
alexdesiqueira <alex.desiqueira@igdore.org>
2021-07-29 14:48:53 -0700

    Fixing tests for ruler

--- butterfly/tests/test_ruler_detection.py
+++ butterfly/tests/test_ruler_detection.py
@@ -7,7 +7,7 @@ RULER_TOP = 50
 RULER_LEFT = 0.2
 RULER_RIGHT = 0.4
 
-def test_binarize():
+def test_binarize_ruler():
     input = np.array([[[1, 1, 1],
                       [2, 2, 2]],
                      [[1, 1, 1],
@@ -15,7 +15,7 @@ def test_binarize():
                      [[1, 1, 1],
                       [1, 1, 1]]])
     output = np.array([[False, True], [False, True], [False, False]])
-    nt.assert_equal(rd.binarize(input), output)
+    nt.assert_equal(rd.binarize_ruler(input), output)
 
 
 def test_fourier():
@@ -30,16 +30,9 @@ def test_fourier():
     nt.assert_almost_equal(T_space, T_SMALL, decimal=0)
 
 
-def test_binary_rect():
-    data = np.full((500, 500, 3), 0.0)
-    data[450:500, :, :] += 0.6
-    expected = np.full((50, 100, 3), 0.6)
-    nt.assert_equal(rd.binarize_rect(450, data), expected)
-
-
 def test_main_ruler_detection():
     data = np.ones((500, 500, 3))
     data[350:500, 0:500:20, :] = 0
     data[425:500, 10:500:20, :] = 0
-    t_space, top_space = rd.main(data)
+    t_space, top_space = rd.main(data, data[:, :, 0])
     nt.assert_almost_equal(t_space, 20, decimal=0)
